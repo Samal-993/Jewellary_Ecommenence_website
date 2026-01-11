9aa91db2b00c4271c2fa0a4b067dc2feab892d95
dipaksamal993@gmail.com <dipaksamal993@gmail>
2026-01-11 11:03:33 +0530

    axiox update

--- frontend/src/api/axios.jsx
+++ frontend/src/api/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: "https://jewellary-ecommenence-website.vercel.app/api",
 });
 
 API.interceptors.request.use(
